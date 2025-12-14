03c5b18e0f3992326cb5f20cdeb530040ab6a69b
rlosano-web <rlosano@pnw.edu>
2025-12-14 12:58:32 -0600

    Removed welcoming message on main.c in case it affects the automated testing.

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
     (void)argc; // Unused
     (void)argv; // Unused
     
-    printf("=== MyShell - A Mini Unix Shell for CS-12600 ===\n");
+
     
     // Create shell instance
     Shell* shell = create_shell();
